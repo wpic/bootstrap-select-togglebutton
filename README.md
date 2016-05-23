0f345c3432b11822796bdf9ef962de03e3295b96
Juliette TRAN <jtran@localhost.localdomain>
2016-05-23 14:45:39 +0800

    Update README, upload screenshot, fix select bug

---
+++ README.md
@@ -0,0 +1,20 @@
+bootstrap-select-togglebutton
+=============================
+
+A simple plugin jQuery to convert select boxes to Bootstrap toggle button group.
+
+![togglebutton screenshot](screenshot.png)
+
+Usage
+=====
+
+Create a select element and use the plugin.
+
+`$('select').togglebutton();`
+
+You can also check it out on the [example file](example.html).
+
+Options
+=======
+
+No options for this version.
--- select-togglebutton.js
+++ select-togglebutton.js
@@ -3,31 +3,33 @@
     $.fn.togglebutton = function(opts) {
 
         var settings = $.extend( {}, $.fn.togglebutton.defaults, opts);
+        this.each(function() {
+            var self = $(this);
+            var options = self.children('option');
+            var buttons = options.map(function(index, opt) {
+                var button = $("<button type='button' class='btn btn-default'></button>")
+                  .prop('value', opt.value)
+                  .text(opt.text);
 
-        var self = this;
-        var options = self.find('option');
-        var buttons = options.map(function(index, opt) {
-            var button = $("<button type='button' class='btn btn-default'></button>")
-              .prop('value', opt.value)
-              .text(opt.text);
-
-            return button[0];
-        });
-
-        buttons.each(function(index, btn) {
-            $(btn).click(function() {
-                $(btn).addClass("active");
-                $(btn).siblings(".active").removeClass("active");
-                self.val(btn.value);
+                return button[0];
             });
-        });
 
-        var btnGroup = $("<div class='btn-group'>").append(buttons);
-        self.after(btnGroup);
-        self.hide();
+            buttons.each(function(index, btn) {
+                $(btn).click(function() {
+                    $(btn).addClass("active");
+                    $(btn).siblings(".active").removeClass("active");
+                    self.val(btn.value);
+                });
+            });
+
+            var btnGroup = $("<div class='btn-group'>").append(buttons);
+            self.after(btnGroup);
+            self.hide();
+        });
     };
 
     $.fn.togglebutton.defaults = {
 
     };
+
 }(jQuery));
